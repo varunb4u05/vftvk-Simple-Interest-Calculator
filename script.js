7245f78750bf99161a5120fbfd745a1cbf56db56
Varun <varunvarmadatla@gmail.com>
2021-09-30 15:46:29 +0000

    added % to rate

--- script.js
+++ script.js
@@ -26,6 +26,6 @@ function compute()
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
